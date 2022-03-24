ec59d992f8aebf49bebc9535a8c9372f70e19eda
Annabelle <annabelle.jeffre@gmail.com>
2022-03-24 09:46:50 +0100

    added some sass, changed contac, and booking.Émail should work

--- index.js
+++ index.js
@@ -1,84 +1,81 @@
-const express = require('express')
-const app = express()
-const formidable = require('formidable')
-const sendEmail = require('./sendEmail')
-const dotenv = require('dotenv').config()
+const express = require('express');
+const app = express();
+const formidable = require('formidable');
+const sendEmail = require('./sendEmail');
+const dotenv = require('dotenv').config();
 
-
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 /* app.use(express.urlencoded({ extended: true })) */
 
 //ROUTING
 app.get('/', (req, res) => {
-    console.log(sendEmail)
-    res.render('pages/home')
-})
+    console.log(sendEmail);
+    res.render('pages/home');
+});
 app.get('/contact', (req, res) => {
-    res.render('pages/contact')
-})
+    res.render('pages/contact');
+});
 app.get('/receivedmessage', (req, res) => {
-    res.render('pages/receivedmessage', { userMessage })
-})
+    res.render('pages/receivedmessage', { userMessage });
+});
 app.get('/booking', (req, res) => {
-    res.render('pages/booking')
-})
+    res.render('pages/booking');
+});
 app.get('/receivedbooking', (req, res) => {
-    res.render('pages/receivedbooking', { userBooking })
-})
+    res.render('pages/receivedbooking', { userBooking });
+});
 app.get('/menue', (req, res) => {
-    res.render('pages/menue')
-})
+    res.render('pages/menue');
+});
 app.get('/galerie', (req, res) => {
-    res.render('pages/galerie')
-})
+    res.render('pages/galerie');
+});
 app.get('/notFound', (req, res) => {
-    res.render('pages/notFound')
-})
+    res.render('pages/notFound');
+});
 app.get('/events', (req, res) => {
-    res.render('pages/events')
-})
+    res.render('pages/events');
+});
 app.get('/team', (req, res) => {
-    res.render('pages/team')
-})
+    res.render('pages/team');
+});
 app.get('/feedback', (req, res) => {
-    res.render('pages/feedback')
-})
-
+    res.render('pages/feedback');
+});
 
 //DatenArrays
-const userMessage = []
-const userBooking = []
+const userMessage = [];
+const userBooking = [];
 
 //get FormData
 app.post('/contact', (req, res) => {
     const form = formidable({ multiples: true });
-    console.log('Ich bin in app.post/contact')
+    console.log('Ich bin in app.post/contact');
     //gib die geschrieben Nachricht zurück plus anderer Satz
     form.parse(req, (err, fields, files) => {
         if (err) {
-            console.log(err)
-            res.render('pages/notFound')
+            console.log(err);
+            res.render('pages/notFound');
         } else {
             const incomingMessage = {
                 name: fields.name,
                 email: fields.email,
-                phone: fields.phone,
-                message: fields.message
-            }
-            console.log('Incoming:', incomingMessage)
-            userMessage.push(incomingMessage)
+                message: fields.message,
+            };
+            console.log('Incoming:', incomingMessage);
+            userMessage.push(incomingMessage);
             res.redirect('/receivedmessage');
         }
     });
-})
+});
 
 app.post('/booking', (req, res) => {
     const form = formidable();
 
     form.parse(req, (err, fields) => {
         if (err) {
-            res.render('pages/notFound')
+            res.render('pages/notFound');
             return;
         } else {
             const incomingBooking = {
@@ -87,20 +84,15 @@ app.post('/booking', (req, res) => {
                 phone: fields.phone,
                 people: fields.people,
                 date: fields.date,
-                time: fields.time
-            }
-            console.log(incomingBooking)
-            userBooking.push(incomingBooking)
-            sendEmail(incomingBooking)
+                time: fields.time,
+            };
+            console.log(incomingBooking);
+            userBooking.push(incomingBooking);
+            sendEmail(incomingBooking);
             res.redirect('/receivedbooking');
-        };
-    })
-})
+        }
+    });
+});
 
-
-
-
-
-
-const PORT = 3141
-app.listen(PORT, () => console.log('Listening on port:', PORT))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Listening on port:', PORT));
